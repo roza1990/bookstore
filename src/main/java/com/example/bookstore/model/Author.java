be833ca0e038bccbd7b034e6f29fbc5251233c7f
roza1990 <rozatahmazyan1@gmail.com>
2019-07-03 01:42:08 +0400

    Update Author.java

--- src/main/java/com/example/bookstore/model/Author.java
+++ src/main/java/com/example/bookstore/model/Author.java
@@ -22,17 +22,17 @@ public class Author {
     @Column
     private int id;
     @Column
-    @NotEmpty(message = "Name may not be empty")
+   
     private String name;
     @Column
-    @NotEmpty(message = "Surname may not be empty")
+   
     private String surname;
     @Column
-    @NotEmpty(message = "Email may not be empty")
-    @Email
+    
+  
     private String email;
     @Column
-    @NotEmpty(message = "Age may not be empty")
+   
     private int age;
 
 }
